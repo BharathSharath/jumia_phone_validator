f5ccc377337de152a29ec151226974ab61552a42
BharathSharath <103111105+BharathSharath@users.noreply.github.com>
2023-06-07 13:02:32 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,16 +1,38 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/database_name'
+db = SQLAlchemy(app)
 
-@app.route('/validate', methods=['POST'])
+class PhoneNumber(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    phone_number = db.Column(db.String(20), nullable=False)
+    is_valid = db.Column(db.Boolean, nullable=False)
+
+    def __init__(self, phone_number, is_valid):
+        self.phone_number = phone_number
+        self.is_valid = is_valid
+
+@app.route('/validate', methods=['GET', 'POST'])
 def validate_phone_number():
-    phone_number = request.json.get('phone_number')
+    if request.method == 'POST':
+        phone_number = request.json.get('phone_number')
+    elif request.method == 'GET':
+        phone_number = request.args.get('phone_number')
+
     country_code = get_country_code(phone_number)
     is_valid = validate(phone_number, country_code)
+
+    phone = PhoneNumber(phone_number=phone_number, is_valid=is_valid)
+    db.session.add(phone)
+    db.session.commit()
+
     result = {
         'phone_number': phone_number,
         'is_valid': is_valid
     }
+
     return jsonify(result)
 
 def get_country_code(phone_number):
